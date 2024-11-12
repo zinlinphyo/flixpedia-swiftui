e6c9cd1d784037cfa1172123115fb17ae46e17e4
Zin Lin Phyo <zinlinphyo.dev@gmail.com>
2024-11-12 22:08:24 +0700

    Initial Commit

---
+++ flixpedia-swiftui/flixpedia_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  flixpedia_swiftuiApp.swift
+//  flixpedia-swiftui
+//
+//  Created by Zin Lin Phyo on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct flixpedia_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
